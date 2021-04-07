bc783f834a2b10f3481262df827e2598051c0f45
Zackory Kelly <zackkelly@live.ca>
2021-04-07 20:40:31 +0000

    Implemented wordsearch to work forwards and backwards

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word) || l.includes(word.split('').reverse().join(''))) return true;
+  }
+  for (let col = 0; col < letters.length; col++) {
+    let verticalString = '';
+    for (let row = 0; row <= letters[col].length; row++) {
+      verticalString += letters[row][col];
     }
-}
+    if (verticalString === word || verticalString === word.split('').reverse().join('')) {
+      return true;
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+
